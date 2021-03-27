2386f4b757d1cb87a56da068f67ee597d7aaf2a7
jiangyugang <email@jiangyg.com>
2021-03-27 17:25:00 +0800

    init

---
+++ src/com/jiang/Main.java
@@ -0,0 +1,15 @@
+package com.jiang;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("12312");
+        sss("asd");
+    }
+
+    public static void sss(String sddsf) {
+
+    }
+
+}
